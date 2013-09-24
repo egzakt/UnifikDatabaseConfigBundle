c7bc91198a27895a2b06177f5b81269d1d54df5b
Hubert Perron <hubert.perron@gmail.com>
2013-09-24 10:09:16 -0400

    Adding an index action and a browsable list of bundles

--- Controller/ConfiguratorController.php
+++ Controller/ConfiguratorController.php
@@ -3,11 +3,14 @@
 namespace Egzakt\DatabaseConfigBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Config\Definition\ArrayNode;
 use Symfony\Component\Config\Definition\NodeInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 
+use Egzakt\DatabaseConfigBundle\Entity\Extension;
 use Egzakt\DatabaseConfigBundle\Form\ConfiguratorType;
 
 /**
@@ -15,15 +18,26 @@ use Egzakt\DatabaseConfigBundle\Form\ConfiguratorType;
  */
 class ConfiguratorController extends Controller
 {
+
     /**
-     * List all config of a given extension
+     * @return Response
+     */
+    public function indexAction()
+    {
+        return $this->render('EgzaktDatabaseConfigBundle::index.html.twig', array(
+            'bundles' => $this->getConfiguratorEnabledBundles()
+        ));
+    }
+
+    /**
+     * Display a form to edit the configuration of a bundle
      *
      * @param Request $request
      * @param string $bundleName
      *
      * @return Response
      */
-    public function listAction(Request $request, $bundleName)
+    public function editAction(Request $request, $bundleName)
     {
         $manager = $this->getDoctrine()->getManager();
         $extensionRepository = $this->getDoctrine()->getRepository('EgzaktDatabaseConfigBundle:Extension');
@@ -31,6 +45,11 @@ class ConfiguratorController extends Controller
 
         $tree = $this->getConfigNode($bundleName);
         $extension = $extensionRepository->findOneByName($tree->getName());
+
+        if (false == $extension) {
+            $extension = new Extension();
+            $extension->setName($tree->getName());
+        }
 
         $form = $this->createForm(new ConfiguratorType(), $extension, array('tree' => $tree, 'request' => $request));
 
@@ -50,8 +69,9 @@ class ConfiguratorController extends Controller
             }
         }
 
-        return $this->render('EgzaktDatabaseConfigBundle::list.html.twig', array(
-            'form' => $form->createView()
+        return $this->render('EgzaktDatabaseConfigBundle::edit.html.twig', array(
+            'form' => $form->createView(),
+            'bundles' => $this->getConfiguratorEnabledBundles()
         ));
     }
 
@@ -65,9 +85,71 @@ class ConfiguratorController extends Controller
         $bundles = $this->get('kernel')->getBundles();
         $bundle = $bundles[$bundleName];
         $extension = $bundle->getContainerExtension();
-        $configuration = $extension->getConfiguration(array(), new ContainerBuilder());
-        $tree = $configuration->getConfigTreeBuilder()->buildTree();
 
-        return $tree;
+        if ($extension) {
+            $configuration = $extension->getConfiguration(array(), new ContainerBuilder());
+            $tree = $configuration->getConfigTreeBuilder()->buildTree();
+
+            return $tree;
+        }
     }
+
+    /**
+     * Check each bundle currently loaded in the kernel and validate configurator support
+     *
+     * -
+     *
+     * @return array
+     */
+    private function getConfiguratorEnabledBundles()
+    {
+        $enabledBundles = array();
+        $bundles = $this->get('kernel')->getBundles();
+
+        foreach ($bundles as $name => $bundle) {
+            try {
+                if ($tree = $this->getConfigurationTree($bundle)) {
+                    if ($tree && $this->isConfiguratorEnabledNode($tree)) {
+                        $enabledBundles[] = $name;
+                    }
+                }
+            } catch (\Exception $e) {
+                // skip error'd bundle
+            }
+        }
+
+        return $enabledBundles;
+    }
+
+    private function getConfigurationTree(BundleInterface $bundle)
+    {
+        $extension = $bundle->getContainerExtension();
+
+        if ($extension) {
+            $configuration = $extension->getConfiguration(array(), new ContainerBuilder());
+            if ($configuration) {
+                return $configuration->getConfigTreeBuilder()->buildTree();
+            }
+        }
+    }
+
+    /**
+     * Check if a configuration node is configuration enabled
+     *
+     * @param NodeInterface $arrayNode
+     *
+     * @return bool
+     */
+    private function isConfiguratorEnabledNode(NodeInterface $arrayNode)
+    {
+        foreach ($arrayNode->getChildren() as $node) {
+            if ($node->getAttribute('configurator')) {
+                return true;
+            } elseif ($node instanceof ArrayNode) {
+                return $this->isConfiguratorEnabledNode($node);
+            }
+        }
+    }
+
+
 }
